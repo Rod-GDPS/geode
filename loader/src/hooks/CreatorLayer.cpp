acd50d14e9148edfa7a62b211949a3d14b3c3950
OmgRod <rlimafonseca2010@gmail.com>
2024-11-23 17:21:45 +0000

    added clans button

--- loader/src/hooks/CreatorLayer.cpp
+++ loader/src/hooks/CreatorLayer.cpp
@@ -11,13 +11,10 @@ class $modify(CreatorLayer) {
 
         if (auto menu = this->getChildByID("creator-buttons-menu")) {
             auto button = CCMenuItemSpriteExtra::create(
-                CCSprite::create("GE_statsBtn.png"_spr),
+                CCSprite::create("GE_clansBtn.png"_spr),
                 this,
                 menu_selector(CreatorLayer::onBack)
             );
-
-            button->setPosition(0.0f, 0.0f);
-            button->setScale(.8f);
 
             menu->addChild(button);
         }
